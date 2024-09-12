f2d0f524fd0a0ad1b5c36e27d4d5546f81106620
michaelisesele <michaelisesele@gmail.com>
2024-09-12 21:33:30 +0100

    move API Key to base URL

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,11 +1,37 @@
+import utils.spotlessConfig
+import java.util.Properties
+
 plugins {
     `core-module-config`
     alias(libs.plugins.hilt.plugin)
     alias(libs.plugins.kotlinSerialization)
 }
 
+val keystoreFile: File = project.rootProject.file("gradle.properties")
+val properties = Properties()
+properties.load(keystoreFile.inputStream())
+
+spotlessConfig(libs.versions.spotless.ktlint)
+
+val newsApiKey = properties.getProperty("NEWS_API_KEY") ?: ""
+val baseUrl = properties.getProperty("NEWS_API_BASE_URL") ?: ""
+
 android {
     namespace = "com.michael.core.network"
+
+    defaultConfig {
+        buildConfigField(
+            type = "String",
+            name = "API_KEY",
+            value = newsApiKey
+        )
+
+        buildConfigField(
+            type = "String",
+            name = "NEWS_API_BASE_URL",
+            value = baseUrl
+        )
+    }
 }
 
 dependencies {
